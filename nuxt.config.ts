923cd3ef5ea4e1b68f76f734ac8a028f8409ac36
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2025-06-19 16:23:36 -0400

    chore: update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/image',
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/test-utils'
-  ]
-})
+  modules: ["@nuxt/image", "@nuxt/content", "@nuxt/eslint", "@nuxt/test-utils"],
+
+  // Runtime configuration for environment variables
+  runtimeConfig: {
+    // Private keys (only available on server-side)
+    supabaseUrl: process.env.SUPABASE_URL,
+    supabaseKey: process.env.SUPABASE_KEY,
+
+    // Public keys (exposed to client-side)
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      // Note: Only use anon/public keys in public config
+      supabaseAnonKey: process.env.SUPABASE_KEY,
+    },
+  },
+});
