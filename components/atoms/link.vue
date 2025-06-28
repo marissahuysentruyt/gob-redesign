9b6a3f6c8e44d5fadd82c29974029c4c5c44ebc3
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2025-06-28 15:21:59 -0400

    feat: link component

---
+++ components/atoms/link.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { LinkProps } from "~/types/index";
+
+const props = withDefaults(defineProps<LinkProps>(), {
+  url: "/", // defaults to homepage if no url is provided
+  isActive: false,
+});
+
+// Add the missing isActive logic
+const route = useRoute();
+const isActive = computed(() => route.path === props.url);
+</script>
+
+<template>
+  <NuxtLink
+    class="cmp-link"
+    :class="{ 'cmp-link--active': isActive }"
+    :to="props.url"
+  >
+    <span class="cmp-link__text">
+      <slot />
+    </span>
+  </NuxtLink>
+</template>
+
+<style scoped>
+.cmp-link {
+  color: red;
+}
+</style>
