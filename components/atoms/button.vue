6ea0f254b538d0e799f7440ab0757ba1cffc1360
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2025-06-28 14:32:52 -0400

    feat: new button component

---
+++ components/atoms/button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="cmp-button">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.cmp-button {
+  --button-text-color: #ffffff;
+  --button-background-color: #5925ab;
+
+  color: var(--button-text-color);
+  background-color: var(--button-background-color);
+  appearance: none;
+  border: none;
+}
+</style>
