16678e7c613daabc9f032c93b7c082ddf62160e1
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2025-06-28 14:08:55 -0400

    feat: add global banner and navigation components

---
+++ components/globals/banner.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="cmp-banner">
+    <div className="cmp-banner__content">
+      <span>📞 248-430-4980</span>
+      <span> 🗺️ 775 E. 14 Mile Rd, Clawson, MI 48017</span>
+    </div>
+    <button class="cmp-banner__button">
+      <!-- <User className="w-4 h-4 mr-2" /> -->
+      Account
+    </button>
+    <button class="cmp-banner__button">
+      <!-- <ShoppingCart className="w-4 h-4 mr-2" /> -->
+      Cart
+      <!-- <Badge className="ml-2 bg-purple-600">0</Badge> -->
+    </button>
+  </section>
+</template>
---
+++ components/globals/navigation.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="cmp-headerNav">
+    <ul class="cmp-headerNav__list">
+      <li><a href="#" class="cmp-headerNav__item">About</a></li>
+      <li class="cmp-headerNav__item">
+        Games
+        <select class="cmp-headerNav__item--dropdown">
+          <option value="game1">Board games</option>
+          <option value="game2">Card games</option>
+          <option value="game3">Role-playing games</option>
+          <option value="game4">Collectible card games</option>
+          <option value="game5">Family games and puzzles</option>
+          <option value="game6">Miniatures</option>
+          <option value="game7">Used games</option>
+          <option value="game8">Game accessories</option>
+          <option value="game9">Pre-order Plus</option>
+        </select>
+      </li>
+      <li class="cmp-headerNav__item">
+        Books
+        <select class="cmp-headerNav__item--dropdown">
+          <option value="game1">Comics</option>
+          <option value="game2">Graphic novels</option>
+          <option value="game3">Bundles</option>
+          <option value="game4">Manga</option>
+          <option value="game5">Comic supplies</option>
+        </select>
+      </li>
+      <li class="cmp-headerNav__item">
+        Resources
+        <select class="cmp-headerNav__item--dropdown">
+          <option value="game1">Board game rental</option>
+          <option value="game2">Private game room rental</option>
+          <option value="game3">Sell your board games</option>
+          <option value="game4">Rewards program</option>
+          <option value="game5">Comic collectors program</option>
+          <option value="game6">GOB News</option>
+          <option value="game7">GOB Persons of Interest</option>
+        </select>
+      </li>
+      <li><a href="#" class="cmp-headerNav__item">Events</a></li>
+      <li><a href="#" class="cmp-headerNav__item">Login</a></li>
+    </ul>
+  </nav>
+</template>
