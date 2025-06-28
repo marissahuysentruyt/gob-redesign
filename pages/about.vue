be3ca4245f68feedb139b1286d5145275607407c
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2025-06-28 10:09:43 -0400

    feat: new layout and pages
    - adds pages/index.vue as the entry point alongside app.vue
    - creates a default, generic layout
    - creates a generic about page
    - all pages respond to manual URL navigation changes

---
+++ pages/about.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="about">
+    <h1>About The GOB Retail Group</h1>
+    <p>
+      Welcome to the Guild of Blades Retail Group (GOB Retail). GOB Retail is a
+      full line hobby game store, print on demand printer and e-commerce web
+      store. The store was started as a unique venture in hobby games retailing,
+      striving to bring a new level of technological application to the business
+      of brick and mortar games retailing. Leveraging the power of a
+      sophisticated e-store with an expanding inventory of over 35,000 new and
+      used games and items all stocked at local retail together with
+      ground-breaking print on demand (POD) printing capabilities to enable
+      independant designers to bring a vast array of unique games to market, GOB
+      Retail is seeking to evolve and expand the adventure games hobby and
+      industry.
+    </p>
+    GOB Retail Stores: GOB Retail launched its first brick and mortar retail
+    store in late 2008. Located in Madison Heights, MI (near Detroit), the GOB
+    Retail store in Madison Heights is a full line hobby game store that stocks
+    board games, card games, magic the gathering, role playing games, table top
+    miniature games, war games, dice, used video games, graphic novels and
+    Osprey Books. More than 35000 games and items currently available and
+    expanding. The Madison Heights store also hosts an open gaming area where
+    players can meet up to play games and where game days for Magic the
+    Gathering, Warmachine and others are held. The Madison Heights store is GOB
+    Retail's first store and is the evolving store template from which
+    additional retail locations are planned. GOBRetail.com offers customers and
+    e-store plus a real time searchable inventory at the local store, a Gamer
+    Finder for finding game goups, an event calendar, gift registry, purchase
+    history, forums, game reviews, product tagging, back in stock notifications
+    and more. GOB Retail POD: From its inception, GOB Retail was created to
+    launch and expand Print on Demand services for games; to bring new
+    production and distribution vehicles to market for independant game
+    designers and publishers to utilize. GOB Retail POD operations began in
+    March of 2008 with the first die cut POD Playing Cards printing service in
+    the USA. GOB Retail quickly expanded the POD card market until it hit max
+    production capacity based on existant finishing equipment and has since been
+    in development of new die cutting machinery to take POD card and board game
+    production to the next level. GOB Retail E-Store: The vast majority of hobby
+    games, new and used, stocked at GOB Retail are also available online to
+    order. We maintain real time inventory so you may shop with the confidence
+    that GOB Retail indeed has the item you are looking for, in stock and ready
+    to ship. Click on "Shop Games" to browse or search our online catalog. GOB
+    Publishing: The Guild of Blades Publishing Group began publishing games in
+    1994 and has published over 300 games and accessories since its inception.
+    While GOB Retail and GOB Publishing operate as seperate companies, the two
+    work closely together on developing new POD production systems and in
+    retailing GOB game products. GOB Retail uses the Guild of Blades trademark
+    under license. For information on Guild of Blades Publishing and its games,
+    visit: www.guildofblades.com
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "default",
+});
+</script>
+
+<template>
+  <div class="header">
+    <h1>GOB Retail</h1>
+    <p>Michigan's Largest Retail Game &amp; Comic Store</p>
+  </div>
+</template>
