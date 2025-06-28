f0d3d573a6852da60cb04b62a5a0fb8dc064cbdd
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2025-06-28 15:49:56 -0400

    feat: add heading component

---
+++ components/atoms/heading.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { HeadingProps } from "~/types/index";
+
+const props = withDefaults(defineProps<HeadingProps>(), {
+  level: "1", // defaults to h1
+  title: "Heading", // defaults to "heading"
+});
+</script>
+
+<template>
+  <component :is="`h${props.level}`">
+    {{ props.title }}
+  </component>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+}
+</style>
