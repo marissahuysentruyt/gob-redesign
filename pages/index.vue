48b0253090133c918f48b47eb897ea757c7a25ac
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2025-06-28 14:10:02 -0400

    fix: move homepage content to header

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,5 @@ definePageMeta({
 </script>
 
 <template>
-  <div class="header">
-    <h1>GOB Retail</h1>
-    <p>Michigan's Largest Retail Game &amp; Comic Store</p>
-  </div>
+  <NuxtPage />
 </template>
