c3543a080256ed5a44d0fcadf5df45468cf926a6
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2025-06-28 15:22:43 -0400

    feat: implements new button and link components

--- components/globals/banner.vue
+++ components/globals/banner.vue
@@ -4,14 +4,14 @@
       <span>📞 248-430-4980</span>
       <span> 🗺️ 775 E. 14 Mile Rd, Clawson, MI 48017</span>
     </div>
-    <button class="cmp-banner__button">
+    <AtomsButton class="cmp-banner__button">
       <!-- <User className="w-4 h-4 mr-2" /> -->
       Account
-    </button>
-    <button class="cmp-banner__button">
+    </AtomsButton>
+    <AtomsButton class="cmp-banner__button">
       <!-- <ShoppingCart className="w-4 h-4 mr-2" /> -->
       Cart
       <!-- <Badge className="ml-2 bg-purple-600">0</Badge> -->
-    </button>
+    </AtomsButton>
   </section>
 </template>
--- components/globals/navigation.vue
+++ components/globals/navigation.vue
@@ -1,45 +1,99 @@
 <template>
   <nav class="cmp-headerNav">
     <ul class="cmp-headerNav__list">
-      <li><a href="#" class="cmp-headerNav__item">About</a></li>
+      <li class="cmp-headerNav__item">
+        <AtomsLink url="about">About</AtomsLink>
+      </li>
       <li class="cmp-headerNav__item">
         Games
-        <select class="cmp-headerNav__item--dropdown">
-          <option value="game1">Board games</option>
-          <option value="game2">Card games</option>
-          <option value="game3">Role-playing games</option>
-          <option value="game4">Collectible card games</option>
-          <option value="game5">Family games and puzzles</option>
-          <option value="game6">Miniatures</option>
-          <option value="game7">Used games</option>
-          <option value="game8">Game accessories</option>
-          <option value="game9">Pre-order Plus</option>
+        <select class="cmp-headerNav__itemDropdown">
+          <option value="game1" class="cmp-headerNav__itemDropdown-item">
+            <AtomsLink url="board-games">Board games</AtomsLink>
+          </option>
+          <option value="game2" class="cmp-headerNav__itemDropdown-item">
+            <AtomsLink url="card-games">Card games</AtomsLink>
+          </option>
+          <option value="game3" class="cmp-headerNav__itemDropdown-item">
+            <AtomsLink url="rpg">Role-playing games</AtomsLink>
+          </option>
+          <option value="game4" class="cmp-headerNav__itemDropdown-item">
+            <AtomsLink url="ccg">Collectible card games</AtomsLink>
+          </option>
+          <option value="game5" class="cmp-headerNav__itemDropdown-item">
+            <AtomsLink url="family-games">Family games and puzzles</AtomsLink>
+          </option>
+          <option value="game6" class="cmp-headerNav__itemDropdown-item">
+            <AtomsLink url="miniatures">Miniatures</AtomsLink>
+          </option>
+          <option value="game7" class="cmp-headerNav__itemDropdown-item">
+            <AtomsLink url="used-games">Used games</AtomsLink>
+          </option>
+          <option value="game8" class="cmp-headerNav__itemDropdown-item">
+            <AtomsLink url="game-accessories">Game accessories</AtomsLink>
+          </option>
+          <option value="game9" class="cmp-headerNav__itemDropdown-item">
+            <AtomsLink url="pre-order-plus">Pre-order Plus</AtomsLink>
+          </option>
         </select>
       </li>
       <li class="cmp-headerNav__item">
         Books
-        <select class="cmp-headerNav__item--dropdown">
-          <option value="game1">Comics</option>
-          <option value="game2">Graphic novels</option>
-          <option value="game3">Bundles</option>
-          <option value="game4">Manga</option>
-          <option value="game5">Comic supplies</option>
+        <select class="cmp-headerNav__itemDropdown">
+          <option value="game1" class="cmp-headerNav__itemDropdown-item">
+            <AtomsLink url="comics">Comics</AtomsLink>
+          </option>
+          <option value="game2" class="cmp-headerNav__itemDropdown-item">
+            <AtomsLink url="graphic-novels">Graphic novels</AtomsLink>
+          </option>
+          <option value="game3" class="cmp-headerNav__itemDropdown-item">
+            <AtomsLink url="bundles">Bundles</AtomsLink>
+          </option>
+          <option value="game4" class="cmp-headerNav__itemDropdown-item">
+            <AtomsLink url="manga">Manga</AtomsLink>
+          </option>
+          <option value="game5" class="cmp-headerNav__itemDropdown-item">
+            <AtomsLink url="comic-supplies">Comic supplies</AtomsLink>
+          </option>
         </select>
       </li>
       <li class="cmp-headerNav__item">
         Resources
-        <select class="cmp-headerNav__item--dropdown">
-          <option value="game1">Board game rental</option>
-          <option value="game2">Private game room rental</option>
-          <option value="game3">Sell your board games</option>
-          <option value="game4">Rewards program</option>
-          <option value="game5">Comic collectors program</option>
-          <option value="game6">GOB News</option>
-          <option value="game7">GOB Persons of Interest</option>
+        <select class="cmp-headerNav__itemDropdown">
+          <option value="game1">
+            <AtomsLink url="game-rentals">Board game rentals</AtomsLink>
+          </option>
+          <option value="game2">
+            <AtomsLink url="private-game-room-rentals"
+              >Private game room rentals</AtomsLink
+            >
+          </option>
+          <option value="game3">
+            <AtomsLink url="consignment">Sell your board games</AtomsLink>
+          </option>
+          <option value="game4">
+            <AtomsLink url="rewards">Rewards program</AtomsLink>
+          </option>
+          <option value="game5">
+            <AtomsLink url="comic-collectors"
+              >Comic collectors program</AtomsLink
+            >
+          </option>
+          <option value="game6">
+            <AtomsLink url="gob-news">GOB News</AtomsLink>
+          </option>
+          <option value="game7">
+            <AtomsLink url="persons-of-interest"
+              >GOB Persons of Interest</AtomsLink
+            >
+          </option>
         </select>
       </li>
-      <li><a href="#" class="cmp-headerNav__item">Events</a></li>
-      <li><a href="#" class="cmp-headerNav__item">Login</a></li>
+      <li class="cmp-headerNav__item">
+        <AtomsLink url="events">Events</AtomsLink>
+      </li>
+      <li class="cmp-headerNav__item">
+        <AtomsLink url="login">Login</AtomsLink>
+      </li>
     </ul>
   </nav>
 </template>
